54238202e3b1bee37c0f416be730ca36fff6dfbc
ale14890591 <ale14890591@yandex.ru>
2014-10-18 17:46:43 +0300

    no message

--- Task 2 2/Text.cs
+++ Task 2 2/Text.cs
@@ -9,22 +9,29 @@ namespace Task_2_2
 {
     public class TextWork
     {
-        private List<StringBuilder> text;
+        private List<List<StringBuilder>> text;
 
-        public List<StringBuilder> Text
+        public List<List<StringBuilder>> Text
         {
             get { return text; }
         }
 
         public TextWork(string path)
         {
-            text = new List<StringBuilder>();
+            text = new List<List<StringBuilder>>();
 
             StreamReader reader = new StreamReader(path);
 
             while (!reader.EndOfStream)
             {
-                text.Add(new StringBuilder(reader.ReadLine()));
+                List<StringBuilder> x = new List<StringBuilder>();
+
+                for (int i = 0; i < 2; i++)
+                {
+                    x.Add(new StringBuilder(reader.ReadLine()));
+                }
+
+                text.Add(x);
             }
         }
     }
---
+++ Task 2 2/WordList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_2
+{
+    public class WordList
+    {
+        private class WordListItem
+        {
+            public string Word {get;set;}
+            public int Count {get;set;}
+            public List<int> Pages {get;set;}
+
+            public WordListItem(string word)
+            {
+                this.Word = word;
+                this.Count = 0;
+                this.Pages = new List<int>();
+            }
+        }
+
+        private List<WordListItem> list;
+
+        public WordList()
+        {
+            this.list = new List<WordListItem>();
+        }
+
+        public void AddWord(string word)
+        {
+            this.list.Add(new WordListItem(word));
+        }
+
+        public void WordOccurenceIncrement(int index)
+        {
+            this.list[index].Count++;
+        }
+
+        public void WordOccurencePages(int index, int page)
+        {
+            this.list[index].Pages.Add(page);
+        }
+    }
+}
